dcea0c25304be9211b362944cfade8cc5565eef2
David Winslow <the@machini.st>
2015-05-05 13:44:52 -0700

    rewrote the mergeAdjacentRegions function, named it reduceAdjacentRegions. realized that performing an operation on the list for every iteration was not very efficient. so instead, reduceAdjacentRegions searches for the start and end position of a new region and appends a new region with those properties to a new list - need to do more testing to make sure it will survive large files

--- folding.py
+++ folding.py
@@ -2,6 +2,7 @@ import sublime
 import sublime_plugin
 import re
 import functools
+import cProfile
 
 def matches(needle, haystack, is_re):
     if is_re:
@@ -27,21 +28,13 @@ def filter(v, e, needle, is_re = False):
             if not matches(needle, v.substr(line), is_re):
                 tofilter.remove(line)
 
-def fold(v, e, needle, is_re = False):
+def fold(v, e, needle):
 	regions = v.find_all(needle)
 	regions = mergeAdjacentRegions(0, regions)[1]
-	#print(regions)
-	#def balls(x,y):
-		#if isRegionsAdjacent(x,y):
-			#return sublime.Region(x.a,y.b)
-		#return y
-	#regions = reduce(balls, regions)
-	#print(regions)
-	#functools.reduce(lambda x,y: if isRegionsAdjacent(x.a,y.b): return Region(x.a,y.b)), regions)
 	v.unfold(v.find_all('^.*$'))
 	v.fold(regions)
 
-def reduceRegions(x,y):
+def compareRegions(x,y):
 	if isRegionsAdjacent(x,y):
 		return Region(x.a,y.b)
 
@@ -54,19 +47,52 @@ def mergeAdjacentRegions(i, regions):
 		# perform comparison
 		if isRegionsAdjacent(regions[i], regions[n]):
 			regions[n].a = regions[i].a	# copy l to r 
-			regions.remove(regions[i])	# delete l
+			regions.pop(i)				# delete l
 		else:
 			i += 1
 			mergeAdjacentRegions(i, regions)
 	return i, regions
+
+# a Region describes the start position and end position of a set of characters in the View
+# e.g. [(249,283)]
+def reduceAdjacentRegions(v, e, needle):
+	
+	regionsFound 			= v.find_all( needle )	# search for regions
+	regionsFiltered 		= []					# initialize match list
+	regionHasStartPosition	= 0						# this region's property a is the beginning of our new region
+
+	# iterate through regions in list regionsFound
+	for index in range( len(regionsFound) - 1):
+		
+		# test the current region, with the one immediately to the right
+		# see if the end position of the left region is next to the end position of the right region
+		# e.g. [(249,283),(284,343)]
+		if not isRegionsAdjacent( regionsFound[ index ], regionsFound[ index + 1 ] ) \
+		   or  index + 2 == len( regionsFound ):
+			
+			startPosition	= regionsFound[ regionHasStartPosition ].a
+			endPosition		= regionsFound[ index ].b
+			region 			= sublime.Region( startPosition, regionsFound[ index ].b )
+			regionsFiltered.append( region )
+			regionHasStartPosition = index + 1
+
+def test(v,e,needle):
+	regions = v.find_all(needle)
+	cProfile.run('reduceAdjacentRegions(v, e, needle)')
+	cProfile.run('mergeAdjacentRegions(0, regions)')
 	
 class ShowOnlyCommentsCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		needle = '^((?!\(.*\)).)*$' #this is the opposite of
 		#needle = '\(.*\)'							<-- this
-		fold(self.view, edit, needle, True)
+		fold(self.view, edit, needle)
 
 class ShowProgramOutlineCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
 		needle = '^([XYZIJKABC]|(G1 )).*$'
-		fold(self.view, edit, needle, True)
+		fold(self.view, edit, needle)
+
+class TestCommand(sublime_plugin.TextCommand):
+	def run(self, edit):
+		needle = '^((?!\(.*\)).)*$'
+		test(self.view, edit, needle)
